fa93e0d1c9fc72adaca280c6f6d52665fcccba1f
ElijahBenitez <benitezelijah422@gmail.com>
2025-01-27 21:07:13 -0800

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ *  {Short description of the lab.}
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    printf("Hello world again!\n");
+    return 0;
+}
