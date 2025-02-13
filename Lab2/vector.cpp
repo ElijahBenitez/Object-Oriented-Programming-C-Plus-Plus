4ef9e691c1bc8237284a47cd4099f633def3dde1
ElijahBenitez <benitezelijah422@gmail.com>
2025-02-13 14:25:36 -0800

    created images folder

---
+++ Lab2/vector.cpp
@@ -0,0 +1,13 @@
+/***
+ *  This lab makes sure we can use the terminal to run and create files.
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    printf("Hello world again!\n");
+    return 0;
+}
