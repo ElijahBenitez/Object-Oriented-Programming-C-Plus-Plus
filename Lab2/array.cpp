9522c88853ef26ab96b2808b031297130d652924
ElijahBenitez <benitezelijah422@gmail.com>
2025-02-13 15:46:04 -0800

    lab2

--- Lab2/array.cpp
+++ Lab2/array.cpp
@@ -1,13 +1,36 @@
-/***
- *  This lab makes sure we can use the terminal to run and create files.
- */
-
 #include <iostream>
-#include <cstdlib>
+#include <vector>
 using namespace std;
+/**
+ 
+@brief prints the elements in the array and their memory locations
+@param arr - array of integers
+@param size - the number of elements in the array*/
+void printMemArr(const int * arr, int size) {
+    printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
+    for(int i = 0; i < size; i++) {
+        printf("Value :%i at Memory Location: %p\n", arr[i], arr + i);
+    }
 
-int main(){
-    cout << "Hello world!" << endl;
-    printf("Hello world again!\n");
-    return 0;
+}
+
+void incArrBy10(int * arr, int size) {
+    for(int i = 0; i < size; i++) {
+        arr[i] += 10;
+    }
+}
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE];
+    for(int i = 0; i < SIZE; i++) {
+        arr[i] = 100 + i;
+    }
+printf("Before------------\n");
+printMemArr(arr, SIZE);
+incArrBy10(arr, SIZE);
+printf("After------------\n");
+printMemArr(arr, SIZE);
+
+return 0;
 }
--- Lab2/vector.cpp
+++ Lab2/vector.cpp
@@ -1,13 +1,43 @@
-/***
- *  This lab makes sure we can use the terminal to run and create files.
- */
-
 #include <iostream>
-#include <cstdlib>
+#include <vector>
 using namespace std;
+/**
+* @brief prints the elements in the vector and their memory locations
+*
+* @param v vector of integers
+*/
+void printMemVec(const vector<int>& v) {
+    for (int v : v) {
+        cout << v << " ";
+    }
 
-int main(){
-    cout << "Hello world!" << endl;
-    printf("Hello world again!\n");
-    return 0;
 }
+
+/**
+@brief increments all of the elements in v by 10
+* @param v - address to a vector of integers */
+void incVecBy10(...){
+}
+
+int main() {
+    // create a constant integer called SIZE that is of value 5
+    int SIZE = 5;
+    // create a vector of integers called vec that can hold up to 5 elements
+    vector<int> vec(SIZE);
+    //use a for loop to populate vec with the values 100 to 104
+    for (int i = 0; i < 5; i++) {
+        vec.push_back(i+100);
+    }
+    printf("Before Increment- //call printMemVec(...) on vec//call incBy10(...) on vec -\n");
+    printf("After Increment--\n");
+    //call printMemVec (...) on vec again to view the changes
+    // remove last element of vec
+    // (refer to docs https://cplusplus.com/reference/vector/vector/#google_vignette)
+    printf("After Pop--\n");
+    //call printMemVec(...) on vec again to view the changes
+    // append 101 and 102 at the end of vec
+    // (refer to docs https://cplusplus.com/reference/vector/vector/#google_vignette)
+    printf("After Push‒‒‒‒‒‒‒-\n");
+    //call printMemVec (...) on vec again to view the changes
+    return 0;
+    }
